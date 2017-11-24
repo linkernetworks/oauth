ef5c7fe3f85f69e50dd447101bf457d24044687c
David Chang <chechiachang999@gmail.com>
2017-11-24 15:00:48 +0800

    Hide password. Fix typos
    Former-commit-id: ac19059fcd99e75f9d75710e970b6a4b6c854900

--- entity/user.go
+++ entity/user.go
@@ -13,10 +13,10 @@ type User struct {
 	ID                    bson.ObjectId `bson:"_id" json:"id"`
 	SerialNumber          string        `bson:"serial_number" json:"serial_number"`
 	Email                 string        `bson:"email" json:"email"`
-	Password              string        `bson:"password" json:"password"`
+	Password              string        `bson:"password,omitempty" json:"password,omitempty"`
 	FirstName             string        `bson:"first_name" json:"first_name"`
 	LastName              string        `bson:"last_name" json:"last_name"`
-	CountryCode           string        `bson"country_code" json:"country_code"`
+	CountryCode           string        `bson:"country_code" json:"country_code"`
 	Cellphone             string        `bson:"cellphone" json:"cellphone"`
 	Verified              bool          `bson:"verified" json:"verified"`
 	VerificationCode      string        `bson:"verification_code" json:"verification_code"`
@@ -24,8 +24,8 @@ type User struct {
 	AccessToken           string        `bson:"access_token" json:"access_token"`
 	AccessTokenExpiryTime int64         `bson:"access_token_expiry_time" json:"access_token_expiry_time"`
 	RefreshToken          string        `bson:"refresh_token" json:"refresh_token"`
-	CreatedAt             int64         `bson:"created_at" json:"created_at"`
-	UpdatedAt             int64         `bson:"updated_at" json:"updated_at"`
+	CreatedAt             int64         `bson:"created_at,omitempty" json:"created_at"`
+	UpdatedAt             int64         `bson:"updated_at,omitempty" json:"updated_at"`
 }
 
 const USER_TOKEN_LENGTH = 24
