c511463762343c699178bcab2b5b556fabc02995
zyfdegh <zyfdegg@gmail.com>
2018-03-01 17:08:06 +0800

    implement migration script

--- entity/notification_setting.go
+++ entity/notification_setting.go
@@ -1,5 +1,9 @@
 package entity
 
+import (
+	"bitbucket.org/linkernetworks/aurora/src/config"
+)
+
 // NotificationSetting contains flags indicating whether to send SMS / Email to the user,
 // and determine what kind of messages will be sent.
 type NotificationSetting struct {
@@ -11,4 +15,15 @@ type NotificationSetting struct {
 	OnJobFail    bool `json:"on_job_fail" bson:"on_job_fail"`
 	OnJobStop    bool `json:"on_job_stop" bson:"on_job_stop"`
 	OnJobDelete  bool `json:"on_job_delete" bson:"on_job_delete"`
+}
+
+// NewNotificationSettingFrom creates a NotificationSetting from NotificationConfig
+func NewNotificationSettingFrom(c *config.NotificationConfig) NotificationSetting {
+	return NotificationSetting{
+		OnJobStart:   c.OnJobStart,
+		OnJobSuccess: c.OnJobSuccess,
+		OnJobFail:    c.OnJobFail,
+		OnJobStop:    c.OnJobStop,
+		OnJobDelete:  c.OnJobDelete,
+	}
 }
