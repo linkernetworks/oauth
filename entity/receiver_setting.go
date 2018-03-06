a59f98b1e55ac9b67b1562178e56814284c9d581
zyfdegh <zyfdegg@gmail.com>
2018-03-06 16:31:47 +0800

    rename JSON/BSON tags to camel

--- entity/receiver_setting.go
+++ entity/receiver_setting.go
@@ -3,12 +3,12 @@ package entity
 // ReceiverSetting contains flags indicating whether to send SMS / Email to the user,
 // and determine what kind of messages will be sent.
 type ReceiverSetting struct {
-	EnableSMS   bool `json:"enable_sms" bson:"enable_sms"`
-	EnableEmail bool `json:"enable_email" bson:"enable_email"`
+	EnableSMS   bool `json:"enableSMS" bson:"enableSMS"`
+	EnableEmail bool `json:"enableEmail" bson:"enableEmail"`
 
-	WhenJobStart   bool `json:"when_job_start" bson:"when_job_start"`
-	WhenJobSuccess bool `json:"when_job_success" bson:"when_job_success"`
-	WhenJobFail    bool `json:"when_job_fail" bson:"when_job_fail"`
-	WhenJobStop    bool `json:"when_job_stop" bson:"when_job_stop"`
-	WhenJobDelete  bool `json:"when_job_delete" bson:"when_job_delete"`
+	WhenJobStart   bool `json:"whenJobStart" bson:"whenJobStart"`
+	WhenJobSuccess bool `json:"whenJobSuccess" bson:"whenJobSuccess"`
+	WhenJobFail    bool `json:"whenJobFail" bson:"whenJobFail"`
+	WhenJobStop    bool `json:"whenJobStop" bson:"whenJobStop"`
+	WhenJobDelete  bool `json:"whenJobDelete" bson:"whenJobDelete"`
 }
