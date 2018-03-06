ff9a48acc459de821e78f7ba141aa59efa9595f0
zyfdegh <zyfdegg@gmail.com>
2018-03-06 10:40:14 +0800

    nolonger load notification config from JSON

--- entity/notification_setting.go
+++ entity/notification_setting.go
@@ -1,9 +1,5 @@
 package entity
 
-import (
-	"bitbucket.org/linkernetworks/aurora/src/config"
-)
-
 // NotificationSetting contains flags indicating whether to send SMS / Email to the user,
 // and determine what kind of messages will be sent.
 type NotificationSetting struct {
@@ -15,17 +11,4 @@ type NotificationSetting struct {
 	OnJobFail    bool `json:"on_job_fail" bson:"on_job_fail"`
 	OnJobStop    bool `json:"on_job_stop" bson:"on_job_stop"`
 	OnJobDelete  bool `json:"on_job_delete" bson:"on_job_delete"`
-}
-
-// NewNotificationSettingFrom creates a NotificationSetting from NotificationConfig
-func NewNotificationSettingFrom(c *config.NotificationConfig) NotificationSetting {
-	return NotificationSetting{
-		EnableSMS:    c.EnableSMS,
-		EnableEmail:  c.EnableEmail,
-		OnJobStart:   c.OnJobStart,
-		OnJobSuccess: c.OnJobSuccess,
-		OnJobFail:    c.OnJobFail,
-		OnJobStop:    c.OnJobStop,
-		OnJobDelete:  c.OnJobDelete,
-	}
 }
