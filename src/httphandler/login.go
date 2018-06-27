3aa4fc2e85daf82b5d298ae63d1a4ad77ba1f811
Aren Chen <yarencheng@gmail.com>
2018-06-27 08:19:02 -0700

    /api/login

---
+++ src/httphandler/login.go
@@ -0,0 +1,44 @@
+package httphandler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/linkernetworks/logger"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+func Login(c *gin.Context) {
+
+	email := c.Query("email")
+	if email == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"error":   fmt.Sprintf("email is required"),
+		})
+		return
+	}
+
+	password := c.Query("password")
+	if password == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"error":   fmt.Sprintf("password is required"),
+		})
+		return
+	}
+
+	// TODO: verify email/password
+	user_id := email
+	logger.Debugf("user [%v] login", user_id)
+
+	session := sessions.Default(c)
+	session.Set("user_id", user_id)
+	session.Save()
+
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+	})
+}
